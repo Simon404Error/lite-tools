7f1986e4ffcf69b4d13804e4998be8a106358af3
xiyuesaves <xiyuesaves@gmail.com>
2026-09-14 17:02:24 +0800

    fix: 修复日志打印引起的报错

--- src/main/modules/messages/sendMessageInterceptor/convertVideoFileToVideoMsg.ts
+++ src/main/modules/messages/sendMessageInterceptor/convertVideoFileToVideoMsg.ts
@@ -6,13 +6,6 @@ const log = createLogger("convertVideoFileToVideoMsg");
 
 function convertVideoFileToVideoMsg(msgPayload: any): boolean {
   const msgElement = msgPayload.msgElements[0];
-  log(
-    msgElement,
-    msgElement?.elementType === 3,
-    msgElement?.fileElement,
-    isVideoFile(msgElement.fileElement.filePath),
-    +msgElement.fileElement.fileSize / 1024 / 1024 <= 99,
-  );
   if (
     msgElement &&
     msgElement?.elementType === 3 &&
@@ -20,26 +13,11 @@ function convertVideoFileToVideoMsg(msgPayload: any): boolean {
     isVideoFile(msgElement.fileElement.filePath) &&
     +msgElement.fileElement.fileSize / 1024 / 1024 <= 99
   ) {
-    // const { fileName, filePath, picHeight, picWidth, picThumbPath, fileSize } = msgElement.fileElement;
-    // const videoElement = {
-    //   elementType: ElementType.VideoElement,
-    //   elementId: "",
-    //   videoElement: {
-    //     filePath: filePath,
-    //     fileName,
-    //     videoMd5: md5HexStr,
-    //     thumbMd5: Utils.getFileMD5(oldThumbPath),
-    //     fileSize,
-    //     thumbWidth: picWidth,
-    //     thumbHeight: picHeight,
-    //     thumbPath: picThumbPath,
-    //   },
-    // };
     sendMessage(msgPayload.peer, [
       {
         type: "video",
         path: msgElement.fileElement.filePath,
-        originMsgEl:msgElement.fileElement
+        originMsgEl: msgElement.fileElement,
       },
     ]);
     return true;
